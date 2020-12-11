c3f9c2f99a3bbeff9e80971448a4b3dca228aca3
SaiChand440 <59303987+SaiChand440@users.noreply.github.com>
2020-12-11 08:30:12 +0530

    Final

---
+++ app/src/main/java/com/chandhu/birthdaycards/Companion.kt
@@ -0,0 +1,7 @@
+package com.chandhu.birthdaycards
+
+class Companion {
+    companion object {
+        const val NAME_EXTRA = "name_extra"
+    }
+}
--- app/src/main/java/com/chandhu/birthdaycards/MainActivity.kt
+++ app/src/main/java/com/chandhu/birthdaycards/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.chandhu.birthdaycards
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createBirthdayButton.setOnClickListener {
+            val string : String = nameInput.editableText.toString()
+            startActivity(Intent(this,BirthdayGreetingActivity::class.java).putExtra(Companion.NAME_EXTRA,string))
+        }
     }
 }
